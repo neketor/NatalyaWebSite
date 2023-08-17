87aa57e23ea0ae2aafaf5c18f44168ee207afdc1
neketor <114851541+neketor@users.noreply.github.com>
2023-08-17 11:38:17 +0300

    Добавили наш проект

---
+++ web/static/app.js
@@ -0,0 +1,20 @@
+let num = 53
+let firstName = 'OkPonJiza'
+const isProgrammer = true
+
+// Can Do
+
+// let $ = 'test'
+// let $num = 'TEST'
+// let num$ = 42
+// let _ = 49
+// let _num = 49
+// let num_ = 49
+// let first_name_second = 'pon' // bad
+// let myNum = 34 // good
+// let num42 = 10
+
+// firstName = "Max"
+// isProgrammer = false //  - error because of const!
+
+
